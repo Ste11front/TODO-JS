97a7a2f23c98a97a055aac7c0671446e614295d1
Ste11front <137266358+Ste11front@users.noreply.github.com>
2023-07-10 13:33:01 +0200

    s

---
+++ model/manager.js
@@ -0,0 +1,27 @@
+class Manager{
+    constructor(todoArray = []){
+        this.todoArray = todoArray;
+    }
+
+    addToDo(todo){
+        this.todoArray.push(todo);
+    }
+
+    orderTodosByTitle(){
+        this.todoArray.sort((todo1,todo2)=>todo1.compareByTitle(todo2));
+    }
+
+    orderTodosByDate(){
+        this.todoArray.sort((todo1,todo2)=>todo1.compareByDate(todo2));
+    }
+
+    deleteTodo(index){
+        this.todoArray.splice(index, 1);
+    }
+
+    addTodoWithTitle(title){
+        const newTodo = new Todo(title);
+        this.addToDo(newTodo);
+    }
+
+}
